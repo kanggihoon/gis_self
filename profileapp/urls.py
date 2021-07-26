31fc1951f779d3b4190155083fdf252f945fa715
kanggihoon <65819019+kanggihoon@users.noreply.github.com>
2021-07-26 12:32:45 +0900

    7/26 Django study commit3

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -1,5 +1,9 @@
+from django.urls import path
 
+from profileapp.views import ProFileCreateView
+
+app_name = 'profileapp'
 
 urlpatterns = [
-
+    path('create/', ProFileCreateView.as_view(), name='create')
 ]
--- profileapp/views.py
+++ profileapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from profileapp.forms import ProFileCreationForm
+from profileapp.models import Profile
+
+
+class ProFileCreateView(CreateView):
+    model = Profile
+    form_class = ProFileCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'profileapp/create.html'
