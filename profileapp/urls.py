19f61943d1acaaff11c6203813f47995c38fd7b6
kanggihoon <65819019+kanggihoon@users.noreply.github.com>
2021-07-27 18:08:32 +0900

    7/27 Django study commit4

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from profileapp.views import ProFileCreateView
+from profileapp.views import ProFileCreateView, ProfileUpdateView
 
 app_name = 'profileapp'
 
 urlpatterns = [
-    path('create/', ProFileCreateView.as_view(), name='create')
+    path('create/', ProFileCreateView.as_view(), name='create'),
+    path('update/<int:pk>', ProfileUpdateView.as_view(), name='update'),
 ]
